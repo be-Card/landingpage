ea9104c128d8421a31681253aea7220a0f7b4804
beCard <dev@becard.local>
2026-01-18 19:39:29 -0300

    Keep only landing-web

--- landing-web/vite.config.js
+++ landing-web/vite.config.js
@@ -1,12 +1,6 @@
 import react from '@vitejs/plugin-react';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 export default defineConfig({
-  plugins: [react()],
-  publicDir: path.resolve(__dirname, '..', '.figma')
+  plugins: [react()]
 });
-
